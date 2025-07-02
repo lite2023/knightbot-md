7689f907ab08c363d8df9993110b86c3777ce9e8
lite2023 <128945874+lite2023@users.noreply.github.com>
2025-07-02 17:34:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -99,8 +99,8 @@ const videoCommand = require('./commands/video');
 // Global settings
 global.packname = settings.packname;
 global.author = settings.author;
-global.channelLink = "https://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp3A";
-global.ytch = "Mr Unique Hacker";
+global.channelLink = "https://chat.whatsapp.com/I6dar9z1j26BpIWlsj26ML?mode=r_c";
+global.ytch = "Mr light store";
 
 // Add this near the top of main.js with other global configurations
 const channelInfo = {
@@ -109,7 +109,7 @@ const channelInfo = {
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
             newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterName: 'lightbot MD',
             serverMessageId: -1
         }
     }
