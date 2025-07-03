707d5e55765456dd6e11dea6c55e95646137119b
lite2023 <128945874+lite2023@users.noreply.github.com>
2025-07-03 11:41:04 +0300

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -5,7 +5,7 @@ module.exports = async function (sock, chatId, city) {
         const apiKey = '4902c0f2550f58298ad4146a92b65e10';  // Replace with your OpenWeather API Key
         const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`);
         const weather = response.data;
-        const weatherText = `Weather in ${weather.name}: ${weather.weather[0].description}. Temperature: ${weather.main.temp}°C.`;
+        const weatherText = `الطقس في ${weather.name}: ${weather.weather[0].description}. درجه الحراره: ${weather.main.temp}°C.`;
         await sock.sendMessage(chatId, { text: weatherText });
     } catch (error) {
         console.error('في خطء الطقس مش باين عندي:', error);
