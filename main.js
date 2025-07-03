99b781d7da2b8560d8661bf5bdb1424e5700b833
lite2023 <128945874+lite2023@users.noreply.github.com>
2025-07-03 12:34:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -108,7 +108,7 @@ const channelInfo = {
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
+            newsletterJid: 'I6dar9z1j26BpIWlsj26ML@newsletter',
             newsletterName: 'lightbot MD',
             serverMessageId: -1
         }
